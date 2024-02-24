eb5ff5427f384a35886d9d8f7c83e6e827983930
Dao Ho <daoho2004@gmail.com>
2024-02-24 15:55:02 -0500

    dashboard ui updates

--- finding-fishe/src/app/dashboard/page.js
+++ finding-fishe/src/app/dashboard/page.js
@@ -1,12 +1,31 @@
 "use client";
+import { useRef } from "react";
 
- export default function Dashboard() {
-    return (
-      <div className="w-screen h-screen bg-white">
-        <div className="flex h-full w-full items-center justify-center">
-          <h1 className="text-black text-[2vw] font-bold">Dashboard</h1>
+export default function Dashboard() {
+  const fileInput = useRef(null);
+
+  const handleUpload = () => {
+    console.log("Button clicked!");
+    // trigger the click event of the file input
+    fileInput.current.click();
+  };
+
+  return (
+    <html>
+        <div className="w-screen h-screen bg-[#ecf2fc]">
+          <div
+            id="uploadContainer"
+            className="flex h-[90vh] w-[20vw] rounded-[1vw] bg-white translate-x-[77.5vw] translate-y-[5vh] justify-center"
+          >
+            <button
+              onClick={handleUpload}
+              className="bg-[#164e86] w-[17vw] h-[17vw] mt-[3vh] rounded-[1vw] text-white"
+            >
+              upload
+            </button>
+            <input type="file" ref={fileInput} style={{ display: "none" }} />
+          </div>
         </div>
-      </div>
-    );
-  }
- 
+    </html>
+  );
+}
--- finding-fishe/src/app/page.js
+++ finding-fishe/src/app/page.js
@@ -5,8 +5,8 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-white">
-      <div className="flex h-full w-full items-center justify-center">
-        <h1 className="text-black text-[2vw] font-bold">Finding FishE</h1>
+      <div className="h-[10vh] w-[100vw]">
+        <h1 className="text-black text-[2vw] font-bold">Welcome to Finding FishE</h1>
         <Link  href='/dashboard'> 
         <button class="shadow text-[1.5vw] font-bold bg-[#2f69fd] w-[20vw] py-[1.5vh] rounded w-[20vw] text-white h-auto font-roboto">
           Dashboard
