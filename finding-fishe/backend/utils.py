6ab107c7f955df47c577eae9c68103c1002a88dd
Kloafe <127902759+Kloafe@users.noreply.github.com>
2024-02-23 23:34:46 -0500

    stuff

--- finding-fishe/backend/utils.py
+++ finding-fishe/backend/utils.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+
+def recieve_json():
+    """
+    :param filename: filename of config document
+    :return: a dicitionary of True values and False values
+    """
+
+
+
+
+def
