02da953cd9cc14c362fd219a6bb518b05d013d74
Alina <55455721+agonzalez22@users.noreply.github.com>
2024-02-24 17:04:01 -0500

    adding scoring func

--- finding-fishe/server/utils.py
+++ finding-fishe/server/utils.py
@@ -5,3 +5,19 @@ def word_sorter(json_file):
     :return:
     """
 
+def score_receipt(rec_json): 
+    """ Scores a json obj of a single receipt based on company policy sentiments 
+    Args: 
+        rec_json (json/dict): a json/dct obj containing receipt information 
+    Returns: 
+        json containing the score & the categories in the receipt. 
+    """
+    pass
+
+
+    """ 
+    ex return: 
+    ** Note: we have to flag if we have a non-tax-deductible on the receipt regardless... **
+    # raised indicating a raised flag!
+    {'raised': True, 'score': 0.33, True: ['Food'], False: ['Transportation', 'Office Supplies']}
+    """ 
