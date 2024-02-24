2067814c3498e4928cb5070b8eda095c2354ad5a
Kloafe <127902759+Kloafe@users.noreply.github.com>
2024-02-24 13:42:08 -0500

    setting up server

--- finding-fishe/src/app/layout.js
+++ finding-fishe/src/app/layout.js
@@ -1,17 +1,21 @@
+"use client";
+import React from "react";
 import { Inter } from "next/font/google";
+import Head from 'next/head'; // Import Head from next/head
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Finding FishE",
-  description: "Protecting small businesses from tax fraud",
-};
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <Head>
+        <title>Finding FishE</title>
+        <meta name="description" content="Protecting small businesses from tax fraud" />
+      </Head>
+      <div className={inter.className}>
+        {children}
+      </div>
+    </>
   );
 }
