3721a5f9c8a671b1ad68d6fab4041048b11bcb44
Dao Ho <daoho2004@gmail.com>
2024-02-23 21:39:21 -0500

    initial commit

---
+++ finding-fishe/src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-white">
+      <div className="flex h-full w-full items-center justify-center">
+        <h1 className="text-black text-[2vw] font-bold">Finding FishiE</h1>
+      </div>
+    </div>
+  );
+}
