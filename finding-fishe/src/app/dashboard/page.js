3816491cca65c1e035091a84fd027706465e3acf
Dao Ho <daoho2004@gmail.com>
2024-02-24 14:01:10 -0500

    routing init

---
+++ finding-fishe/src/app/dashboard/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+ function Dashboard() {
+    return (
+      <div className="w-screen h-screen bg-white">
+        <div className="flex h-full w-full items-center justify-center">
+          <h1 className="text-black text-[2vw] font-bold">Dashboard</h1>
+        </div>
+      </div>
+    );
+  }
+  
+  export { Dashboard };
+ 
--- finding-fishe/src/app/page.js
+++ finding-fishe/src/app/page.js
@@ -1,9 +1,20 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-white">
       <div className="flex h-full w-full items-center justify-center">
-        <h1 className="text-black text-[2vw] font-bold">Finding FishiE</h1>
+        <h1 className="text-black text-[2vw] font-bold">Finding FishE</h1>
+        <h1 className="text-black text-[2vw] font-bold">Finding FishE</h1>
+
+        {/* <Link  href={{
+                    pathname: '/dashboard',
+                  }}> 
+        <button class="shadow text-[1.5vw] font-bold bg-[#2f69fd] w-[20vw] py-[1.5vh] rounded w-[20vw] text-white h-auto font-roboto">
+          Dashboard
+        </button> </Link> */}
       </div>
     </div>
   );
 }
+
