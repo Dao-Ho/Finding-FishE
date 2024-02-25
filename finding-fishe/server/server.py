45907bb40a25682f7de0cc8324f3233cfc5076be
Dao Ho <daoho2004@gmail.com>
2024-02-25 06:05:18 -0500

    supabase score upload

--- finding-fishe/server/server.py
+++ finding-fishe/server/server.py
@@ -3,10 +3,15 @@ import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from utils import decode_pdf, policy_bools, score_receipt, Category_Terms
+from supabase import create_client  # Import the Supabase client
 #from utils import receipt_reader
 #create flask app
 app = Flask(__name__)
 
+# Initialize Supabase client
+SUPABASE_URL = "https://ozaymeocdtfecytksppu.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96YXltZW9jZHRmZWN5dGtzcHB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg4NDgxMTAsImV4cCI6MjAyNDQyNDExMH0.RaPchR8AGzUv8HPzRhcICIkOme8x3kZOODcZ3i-MpqI"
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 CORS(app)
 #API get set up
@@ -33,7 +38,7 @@ def receipt_json():
         score = score_receipt(receipt_data, category_terms, policy_bools_dict)
         print(score)
 
-
+        supabase.table('scores').insert([{'score': score}])
         return jsonify({"message": "JSON received successfully!", "yourData": score}), 200
     else:
         return jsonify({"error": "Request must be JSON"}), 400
